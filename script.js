2a5f891ce6fb477cfa4ec3d514dc72fcf89132bc
Jamel <81921407+JGAMIX7@users.noreply.github.com>
2024-02-08 17:23:57 +0100

    v3 or 4, mobile first

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const menu = document.getElementById("menu");
 const toggle = document.getElementById("toggle");
 const close = document.getElementById("close");
+const header = document.getElementById("header");
 
 
 if (toggle) {
   toggle.addEventListener("click", () => {
     menu.classList.add("active");
     toggle.style.display = "none";
+    menu.style.backgroundColor = "var(--black)";
+    header.style.backgroundColor = "var(--black)";
   });
 }
 
@@ -14,5 +17,7 @@ if (close) {
   close.addEventListener("click", () => {
     menu.classList.remove("active");
     toggle.style.display = "block";
+    menu.style.backgroundColor = "var(--almost-black)";
+    header.style.backgroundColor = "var(--almost-black)";
   });
 }
